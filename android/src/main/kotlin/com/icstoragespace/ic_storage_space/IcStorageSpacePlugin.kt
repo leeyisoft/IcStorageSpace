c29c67a6a0497649bf3d111436392c538166a7ab
李源炳 <leeyisoft@qq.com>
2025-07-05 00:21:04 +0800

    升级 gradel 到 8.7

--- android/src/main/kotlin/com/icstoragespace/ic_storage_space/IcStorageSpacePlugin.kt
+++ android/src/main/kotlin/com/icstoragespace/ic_storage_space/IcStorageSpacePlugin.kt
@@ -1,39 +1,34 @@
 package com.icstoragespace.ic_storage_space
 
+import android.app.usage.StorageStats
+import android.app.usage.StorageStatsManager
+import android.content.Context
+import android.os.Environment
+import android.os.Process
+import android.os.StatFs
+import android.os.storage.StorageManager
 import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import android.os.Environment
-import android.os.StatFs
-import android.os.Process
-
-import android.content.Context
-import android.os.storage.StorageManager
-import android.app.usage.StorageStats
-import android.app.usage.StorageStatsManager
-
 import java.io.File
-import java.io.IOException
 import java.util.UUID
 
 /** IcStorageSpacePlugin */
 class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        context = flutterPluginBinding.getApplicationContext()
+        context = flutterPluginBinding.applicationContext
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ic_storage_space")
         channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -51,24 +46,15 @@ class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
-
     private fun getFreeDiskSpaceInBytes(): Long {
         val stat = StatFs(Environment.getExternalStorageDirectory().path)
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
-            return stat.blockSizeLong * stat.availableBlocksLong
-        else
-            return stat.blockSize.toLong() * stat.availableBlocks.toLong()
+        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            stat.blockSizeLong * stat.availableBlocksLong
+        } else {
+            stat.blockSize.toLong() * stat.availableBlocks.toLong()
+        }
     }
 
-    /**
-     * A more accurate way to get Android storage space
-     *  TODO: api compatibility is subject to rigorous testing （minAPI is 26?)
-     *  https://gist.github.com/li-jkwok/e460a042326e8509ada9ec23ae677bdf
-     */
     private fun getTotalDiskSpaceInBytes(): Long {
         val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
         val storageStatsManager = context.getSystemService(Context.STORAGE_STATS_SERVICE) as StorageStatsManager
@@ -78,149 +64,102 @@ class IcStorageSpacePlugin : FlutterPlugin, MethodCallHandler {
             val uuid = volume.uuid?.let { UUID.fromString(it) } ?: StorageManager.UUID_DEFAULT
             totalBytes += storageStatsManager.getTotalBytes(uuid)
         }
-        return totalBytes.toLong()
+        return totalBytes
     }
 
-    /**
-     * 获取当前应用的存储信息 Added in API level 26
-     * Obtain storage information about the current application
-     * start 8.0 Methods are no longer compatible with 7.0
-     * https://developer.android.com/reference/android/app/usage/StorageStats#getDataBytes()
-     */
     private fun storageStats(): Map<String, Long?> {
-        var packageName = context.getPackageName()
+        val packageName = context.packageName
         val storageManager = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
         val storageStatsManager = context.getSystemService(Context.STORAGE_STATS_SERVICE) as StorageStatsManager
         val storageVolumes = storageManager.storageVolumes
-        //获取当前用户
-        var user = Process.myUserHandle()
+        val user = Process.myUserHandle()
         var appBytes = 0L
         var cacheBytes = 0L
         var dataBytes = 0L
+
         for (volume in storageVolumes) {
-            //检测到他是挂载状态的 并且是主要的那个一个
-            if ("mounted".equals(volume.getState()) && volume.isPrimary()) {
+            if ("mounted" == volume.state && volume.isPrimary) {
                 val uuid = volume.uuid?.let { UUID.fromString(it) } ?: StorageManager.UUID_DEFAULT
-                var storageStats = storageStatsManager.queryStatsForPackage(uuid, packageName, user) as StorageStats
-                // App 包括安装包
-                appBytes += storageStats.getAppBytes()
-                // 软件的缓存 (内部缓存 外部缓存 加code_cache 一起返回的)
-                cacheBytes += storageStats.getCacheBytes()
-                // 用户的数据
-                dataBytes += storageStats.getDataBytes()
+                val stats = storageStatsManager.queryStatsForPackage(uuid, packageName, user)
+                appBytes += stats.appBytes
+                cacheBytes += stats.cacheBytes
+                dataBytes += stats.dataBytes
             }
         }
-        val map: MutableMap<String, Long?> = mutableMapOf()
-        map["cacheBytes"] = cacheBytes.toLong()
-        map["dataBytes"] = dataBytes.toLong()
-        map["appBytes"] = appBytes.toLong()
-        return map
+
+        return mapOf(
+            "cacheBytes" to cacheBytes,
+            "dataBytes" to dataBytes,
+            "appBytes" to appBytes
+        )
     }
 
     private fun pathList(path: String?): List<String> {
-        if (path.isNullOrEmpty()) {
-            return getAllFilesAndDirectories(context.filesDir)
+        return if (path.isNullOrEmpty()) {
+            getAllFilesAndDirectories(context.filesDir)
         } else {
-            return getAllFilesAndDirectories(File(path))
+            getAllFilesAndDirectories(File(path))
         }
     }
 
-    /**
-     * 获取特定目录下面所有的目录及文件 路径列表
-     */
     private fun getAllFilesAndDirectories(directory: File): List<String> {
         val filePaths = mutableListOf<String>()
         if (directory.isDirectory) {
             val files = directory.listFiles()
-
-            if (files != null) {
-                for (file in files) {
-                    if (file.isFile) {
-                        filePaths.add(file.absolutePath)
-                    } else if (file.isDirectory) {
-                        filePaths.addAll(getAllFilesAndDirectories(file))
-                    }
+            files?.forEach { file ->
+                if (file.isFile) {
+                    filePaths.add(file.absolutePath)
+                } else if (file.isDirectory) {
+                    filePaths.addAll(getAllFilesAndDirectories(file))
                 }
             }
         }
-        return filePaths;
+        return filePaths
     }
 
     private fun pathBytes(path: String?): Long? {
-        if (path.isNullOrEmpty()) {
-            return null
-        }
-        return  fileBytes(File(path))
+        return if (path.isNullOrEmpty()) null else fileBytes(File(path))
     }
 
-    private fun fileBytes(f: File): Long {
-        var size: Long = 0
-        if (f.isFile) {
-            size += f.length()
-        } else if (f.isDirectory) {
-            val files = f.listFiles()
-            if (files != null) {
-                for (file in files) {
-                    size += if (file.isFile) file.length() else fileBytes(file)
-                }
+    private fun fileBytes(file: File): Long {
+        return if (file.isFile) {
+            file.length()
+        } else {
+            var size = 0L
+            file.listFiles()?.forEach {
+                size += if (it.isFile) it.length() else fileBytes(it)
             }
+            size
         }
-        return size
     }
 
     private fun clearAllCache(): Boolean {
-        try {
-            val cacheDir = context.cacheDir
-            // val filesDir = context.filesDir
-
-            // 清除内部缓存目录中的文件
-            deleteFiles(cacheDir)
-            deleteFiles(context.getCodeCacheDir())
-
-            // 清除内部文件目录中的文件
-            // deleteFiles(filesDir)
-
-            // 如果应用程序有外部缓存目录，也可以清除外部缓存目录中的文件
-            if (android.os.Environment.getExternalStorageState() == android.os.Environment.MEDIA_MOUNTED) {
-                val externalCacheDir = context.externalCacheDir
-                if (externalCacheDir != null) {
-                    deleteFiles(externalCacheDir)
-                }
-            }
-
-            return true // 清理缓存成功
+        return try {
+            deleteFiles(context.cacheDir)
+            deleteFiles(context.codeCacheDir)
+            context.externalCacheDir?.let { deleteFiles(it) }
+            true
         } catch (e: Exception) {
             e.printStackTrace()
-            return false // 清理缓存失败
+            false
         }
     }
 
     private fun deletePath(path: String?): Boolean {
-        if (path.isNullOrEmpty()) {
-            return false
-        }
-        val dir = File(path)
-        deleteFiles(dir)
-        return true
+        if (path.isNullOrEmpty()) return false
+        return deleteFiles(File(path))
     }
 
     private fun deleteFiles(dir: File): Boolean {
-        println("deleteFiles: ${dir.path}") // 打印列表
         try {
             if (dir.isDirectory) {
-                val children = dir.list()
-                children?.forEach { fileName ->
-                    val file = File(dir, fileName)
-                    // 递归删除子目录和文件
-                    deleteFiles(file)
-                }
+                dir.listFiles()?.forEach { deleteFiles(it) }
             }
-            // 删除当前目录
             dir.delete()
-            return true
         } catch (e: Exception) {
             e.printStackTrace()
-            return false // 清理缓存失败
+            return false
         }
+        return true
     }
 }
